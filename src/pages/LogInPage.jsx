5700880bce758853ea15977dcbe5866d747296e9
Handsol <imhandsol@gmail.com>
2025-02-22 03:17:00 +0900

    feat: 로그인 기능 추가 및 로그인 성공/실패 알림 구현

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,5 +1,48 @@
+// LogInPage.jsx
+import { useState } from "react";
+import { login } from "../api/auth";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const logInHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userData = { id, password };
+      const loginResponse = await login(userData);
+
+      if (loginResponse) {
+        alert("로그인 성공");
+      }
+    } catch (error) {
+      alert("로그인 실패");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Log in</h1>
+      <form onSubmit={logInHandler}>
+        <input
+          type="text"
+          name="id"
+          placeholder="Insert Your ID"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Insert Your PW"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
