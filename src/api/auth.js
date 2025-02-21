3a6e01a52cca225ba0948ce30605fbb5e57428c2
Handsol <imhandsol@gmail.com>
2025-02-22 01:28:39 +0900

    feat: 회원가입 기능 추가 및 오류 메세지 콘솔로그 구현

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,6 +4,11 @@ const API_URL = "https://www.nbcamp-react-auth.link";
 
 // 회원가입 로직
 export const register = async (userData) => {
-  const response = await axios.post(`${API_URL}/register`, userData);
-  return response.data;
+  try {
+    const response = await axios.post(`${API_URL}/register`, userData);
+    return response.data;
+  } catch (error) {
+    console.error("Sign up Error => ", error);
+    console.error("Error detail => ", error.response.data);
+  }
 };
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,5 +1,59 @@
+import { useState } from "react";
+import { register } from "../api/auth";
+
 const SignUpPage = () => {
-  return <div>SingUpPage</div>;
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const signUpHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userData = { id, password, nickname };
+      await register(userData);
+      alert("회원가입 성공");
+    } catch (error) {
+      alert("회원가입 실패");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Sign up</h1>
+      <form onSubmit={signUpHandler}>
+        <input
+          type="text"
+          name="id"
+          placeholder="Insert your ID"
+          value={id}
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="nickname"
+          placeholder="Insert your Name"
+          value={nickname}
+          onChange={(e) => {
+            setNickname(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Insert your password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpPage;
