f27950b7a4fd56699b90ee8f184cb0c338fbc96d
Handsol <imhandsol@gmail.com>
2025-02-22 23:39:32 +0900

    feat: Header 컴포넌트에 로그아웃 기능 추가 - 로그인 상태에 따라 버튼 변경

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,13 @@
 import { useNavigate } from "react-router-dom";
+import { logout } from "../../api/auth";
 
 const Header = () => {
   const navigate = useNavigate();
+  const accessToken = localStorage.getItem("accessToken");
+
+  const logOutHandler = () => {
+    logout();
+  };
 
   return (
     <header className="flex flex-col justify-between items-center h-[100px] w-full max-w-[800px] min-w-[250px] mx-auto bg-gray-500 rounded-lg">
@@ -9,8 +15,13 @@ const Header = () => {
         MBTI 테스트
       </h1>
       <div className="flex text-lg font-medium pb-2 gap-4">
-        <button onClick={() => navigate("/login")}>Log in</button>
-        <button onClick={() => navigate("/signup")}>Sign up</button>
+        {accessToken ? (
+          // 로그인 상태일 때는 로그아웃 버튼
+          <button onClick={logOutHandler}>Log out</button>
+        ) : (
+          // 로그인 상태가 아닐 때는 로그인 버튼
+          <button onClick={() => navigate("/login")}>Log in</button>
+        )}
       </div>
     </header>
   );
