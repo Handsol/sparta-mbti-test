52e3cf32acfaa829da7d0daf02b95e72f7be1a3d
Handsol <imhandsol@gmail.com>
2025-02-22 22:21:56 +0900

    style: Header 컴포넌트 레이아웃 수정 - 높이 및 여백 조정, 텍스트 스타일 개선

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,9 +4,11 @@ const Header = () => {
   const navigate = useNavigate();
 
   return (
-    <header className="flex justify-between items-center h-[80px] w-full max-w-[800px] min-w-[300px] mx-auto bg-gray-500">
-      <h1 onClick={() => navigate("/")}>MBTI 테스트</h1>
-      <div>
+    <header className="flex flex-col justify-between items-center h-[100px] w-full max-w-[800px] min-w-[250px] mx-auto bg-gray-500 rounded-lg">
+      <h1 className="text-4xl font-bold pt-4" onClick={() => navigate("/")}>
+        MBTI 테스트
+      </h1>
+      <div className="flex text-lg font-medium pb-2 gap-4">
         <button onClick={() => navigate("/login")}>Log in</button>
         <button onClick={() => navigate("/signup")}>Sign up</button>
       </div>
