e8eabb0319c375a96a7664435a2acb412c01478d
Handsol <imhandsol@gmail.com>
2025-02-22 17:47:20 +0900

    feat: 헤더 타이틀 클릭 시 홈페이지로 이동 기능 추가 및 사이즈 조정

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   const navigate = useNavigate();
 
   return (
-    <header className="flex justify-between items-center">
-      <h1>MBTI 테스트</h1>
+    <header className="flex justify-between items-center h-[80px] w-full max-w-[800px] min-w-[300px] mx-auto bg-gray-500">
+      <h1 onClick={() => navigate("/")}>MBTI 테스트</h1>
       <div>
         <button onClick={() => navigate("/login")}>Log in</button>
         <button onClick={() => navigate("/signup")}>Sign up</button>
