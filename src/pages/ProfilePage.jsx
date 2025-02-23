56d51abdf210f302cae41886127ec29af4fdbf02
Handsol <imhandsol@gmail.com>
2025-02-23 15:49:12 +0900

    feat: 프로필 페이지에서 사용자 아바타 및 닉네임 표시 방식 개선

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,16 +3,18 @@ import useAuthStore from "../zustand/bearsStore";
 
 const ProfilePage = () => {
   const navigate = useNavigate();
-  const { user } = useAuthStore((state) => state);
+  const { avatar, nickname } = useAuthStore((state) => state);
 
   return (
     <div className="flex flex-col">
       <h1>My Profile</h1>
-      <img src="/public/images/default-profile.png" alt="Default Profile" />
-      <p>Nickname : {user?.nickname || "닉네임을 정하지 않았습니다."}</p>
+      <img
+        src={avatar ? avatar : "/images/default-profile.png"}
+        alt="Profile"
+      />
+      <p>Nickname : {nickname || "닉네임을 정하지 않았습니다."}</p>
       <button onClick={() => navigate("/editprofile")}>Edit Profile</button>
     </div>
   );
 };
-
 export default ProfilePage;
