d2ca2ea333b951705841601801fd2dbc24ee1558
Handsol <imhandsol@gmail.com>
2025-02-25 11:44:53 +0900

    feat: 사용자 ID에 따라 테스트 결과를 가져오고 결과를 표시하는 로직 추가

--- src/pages/TestResultPage.jsx
+++ src/pages/TestResultPage.jsx
@@ -1,38 +1,50 @@
-import { useState, useEffect } from "react";
-import { mbtiDescriptions } from "../utils/mbtiCalculator";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { getTestResults } from "../api/testResults";
+import { mbtiDescriptions } from "../utils/mbtiCalculator";
+import useAuthStore from "../zustand/bearsStore";
 
 const TestResultPage = () => {
-  const [results, setResults] = useState([]);
+  const navigate = useNavigate();
+  const [result, setResult] = useState(null);
+  const { userId } = useAuthStore((state) => state);
+  const [description, setDescription] = useState("");
+
+  console.log(userId);
 
   useEffect(() => {
-    const fetchResults = async () => {
-      try {
-        const fetchedResults = await getTestResults();
-        setResults(fetchedResults);
-      } catch (error) {
-        console.error("Error fetching test results:", error);
+    const fetchTestResult = async () => {
+      const results = await getTestResults();
+      const userResult = results.find((result) => result.userId === userId);
+
+      if (userResult) {
+        setResult(userResult);
+        const description = mbtiDescriptions[userResult.result];
+        setDescription(description);
+      } else {
+        alert("결과를 찾을 수 없습니다.");
       }
     };
 
-    fetchResults();
-  }, []);
+    fetchTestResult();
+  }, [userId, navigate]);
 
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-6">테스트 결과</h1>
-
-      <ul>
-        {results.map(
-          (result) =>
-            result.visibility && (
-              <li key={result.id} className="mb-6">
-                <h2 className="text-2xl">{result.mbtiResult}</h2>
-                <p>{mbtiDescriptions[result.mbtiResult]}</p>
-              </li>
-            )
+    <div className="w-full flex flex-col items-center justify-center bg-white">
+      <div className="bg-white rounded-lg p-8 max-w-lg w-full h-full overflow-auto">
+        <h1 className="text-3xl font-bold text-primary-color mb-6">
+          MBTI 결과
+        </h1>
+        {result ? (
+          <div className="mb-4">
+            <h2 className="text-xl font-bold">결과: {result.result}</h2>
+            <p className="mt-2">{description}</p>
+          </div>
+        ) : (
+          <p>테스트를 진행해주세요!</p>
         )}
-      </ul>
+        <button onClick={() => navigate("/")}>다시 테스트하기</button>
+      </div>
     </div>
   );
 };
