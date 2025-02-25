26153f33ba766534c465dfdf912bf6fa754a1fe9
Handsol <imhandsol@gmail.com>
2025-02-25 13:03:07 +0900

    style: 프로필 페이지 스타일링

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -6,16 +6,25 @@ const ProfilePage = () => {
   const { avatar, nickname } = useAuthStore((state) => state);
 
   return (
-    <div className="flex flex-col">
-      <h1>My Profile</h1>
-      <img
-        src={avatar ? avatar : "/images/default-profile.png"}
-        alt="Profile"
-        width="150"
-        height="150"
-      />
-      <p>Nickname : {nickname || "닉네임을 정하지 않았습니다."}</p>
-      <button onClick={() => navigate("/editprofile")}>Edit Profile</button>
+    <div className="h-[650px] w-full max-w-[700px] min-w-[244px] flex flex-col items-center justify-center bg-blue-100">
+      <div className="bg-white rounded-lg p-6 shadow-md w-[90%] max-w-[500px] text-center border border-blue-200">
+        <h1 className="text-3xl font-bold text-blue-800 mb-4">My Profile</h1>
+        <img
+          src={avatar ? avatar : "/images/default-profile.png"}
+          alt="Profile"
+          className="w-[150px] h-[150px] rounded-full border border-blue-300 shadow-sm mb-4 block mx-auto"
+        />
+
+        <p className="text-lg text-gray-700 mb-6">
+          Nickname: {nickname || "닉네임을 정하지 않았습니다."}
+        </p>
+        <button
+          onClick={() => navigate("/editprofile")}
+          className="bg-blue-200 text-blue-800 py-2 px-6 rounded-lg border border-blue-300 hover:bg-blue-300 transition duration-300 shadow-sm hover:shadow-md"
+        >
+          Edit Profile
+        </button>
+      </div>
     </div>
   );
 };
