3f36e0282bb970361b746d4bdea649918abdd7eb
Handsol <imhandsol@gmail.com>
2025-02-25 12:31:19 +0900

    style: 메인 페이지 스타일링

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 const MainPage = () => {
-  return <div>MainPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="w-full flex flex-col items-center justify-center bg-light-blue-100">
+      <div className="bg-white rounded-lg p-8 shadow-xl max-w-lg w-full text-center">
+        <h1 className="text-3xl font-bold text-black mb-6">
+          테스트를 진행해보세요!
+        </h1>
+        <button
+          onClick={() => navigate("/test")}
+          className="bg-primary-color text-black py-2 px-6 rounded-full text-lg border-2 border-primary-color hover:bg-primary-color-dark hover:border-primary-color-dark transition duration-300"
+        >
+          테스트 시작하기
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
